ee394d992adce8307f1408943f5bf77bcc48e111
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-07-21 20:26:05 +0200

    feat: create ORM main class

---
+++ src/ORM.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Sherpa\Orm;
+
+trait ORM
+{
+
+    private static string $table;
+
+    public function __construct()
+    {
+        //
+    }
+
+    public static function findById(mixed $id): self
+    {
+
+    }
+
+}

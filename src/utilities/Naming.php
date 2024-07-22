fe21eba4fd9ff703908158b3ffbb4b62d9cb7b29
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-07-22 16:58:07 +0200

    docs: add `Naming::tableFromModel()` method PHPdoc

--- src/utilities/Naming.php
+++ src/utilities/Naming.php
@@ -7,6 +7,12 @@ use Symfony\Component\String\Inflector\EnglishInflector;
 class Naming
 {
 
+    /**
+     * Convert given model name to its table name.
+     *
+     * @param string $model Model class
+     * @return string Converted table name
+     */
     public static function tableFromModel(string $model): string
     {
         $inflector = new EnglishInflector();
